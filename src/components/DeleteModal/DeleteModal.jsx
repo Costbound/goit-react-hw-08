851562546f3b466b60bb615f207f925fc15f45c8
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-05-17 00:28:15 +0200

    pre final

---
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import css from "./DeleteModal.module.css";
+import { deleteContact } from "../../redux/contacts/operations";
+export default function DeleteModal({ id, name, onClose }) {
+  const dispatch = useDispatch();
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+  return (
+    <div className={css.modal}>
+      <div className={css.wrapper}>
+        <p className={css.para}>
+          Are you sure you want delete contact{" "}
+          <span className={css.nameSpan}>{name}</span>?
+        </p>
+        <div className={css.btnWrapper}>
+          <button type="button" className={css.yesBtn} onClick={handleDelete}>
+            Yes
+          </button>
+          <button type="button" className={css.noBtn} onClick={onClose}>
+            No
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
