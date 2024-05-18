018d91c46ac6994202d8cf10152be063cbb9ce6d
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-05-18 02:57:44 +0200

    final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,79 +1,63 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import Layout from "../Layout/Layout";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Layout from "../Layout/Layout";
 import { Route, Routes } from "react-router";
-import { Link } from "react-router-dom";
-// import RestrictedRoute from "../RestrictedRoute/RestrictedRoute";
-// import PrivateRoute from "../PrivateRoute/PrivateRoute";
-// import { refreshUser } from "../../redux/auth/operations";
-// import { selectIsRefreshing } from "../../redux/auth/selectors";
-// import Loader from "../Loader/Loader";
-// import { lazy } from "react";
+import RestrictedRoute from "../RestrictedRoute/RestrictedRoute";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import { refreshUser } from "../../redux/auth/operations";
+import { selectIsRefreshing } from "../../redux/auth/selectors";
+import Loader from "../Loader/Loader";
+import { lazy } from "react";
 
-// const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
-// const RegistrationPage = lazy(() =>
-//   import("../../pages/RegistrationPage/RegistrationPage")
-// );
-// const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
-// const ContactsPage = lazy(() =>
-//   import("../../pages/ContactsPage/ContactsPage")
-// );
-// const NotFoundPage = lazy(() =>
-//   import("../../pages/NotFoundPage/NotFoundPage")
-// );
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const RegistrationPage = lazy(() =>
+  import("../../pages/RegistrationPage/RegistrationPage")
+);
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() =>
+  import("../../pages/ContactsPage/ContactsPage")
+);
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage")
+);
 
 export default function App() {
-  //   const dispatch = useDispatch();
-  //   const isRefreshing = useSelector(selectIsRefreshing);
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
-  //   useEffect(() => {
-  //     dispatch(refreshUser());
-  //   }, [dispatch]);
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
-  // return isRefreshing ? (
-  return (
-    <div>
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <Layout>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route
-          path="/"
+          path="/register"
           element={
-            <div>
-              Home Page <Link to="/test">Test</Link>
-            </div>
+            <RestrictedRoute
+              component={<RegistrationPage />}
+              redirectTo="/contacts"
+            />
           }
         />
-        <Route path="/test" element={<div>Test Page</div>} />
-        <Route path="*" element={<div>All Page</div>} />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
-  // ) : (
-  //   <Layout>
-  //     <Routes>
-  //       <Route path="/" element={<HomePage />} />
-  //       <Route
-  //         path="/register"
-  //         element={
-  //           <RestrictedRoute
-  //             component={<RegistrationPage />}
-  //             redirectTo="/contacts"
-  //           />
-  //         }
-  //       />
-  //       <Route
-  //         path="/login"
-  //         element={
-  //           <RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />
-  //         }
-  //       />
-  //       <Route
-  //         path="/contacts"
-  //         element={
-  //           <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
-  //         }
-  //       />
-  //       <Route path="*" element={<NotFoundPage />} />
-  //     </Routes>
-  //   </Layout>
-  // );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,19 @@ import App from "./components/App/App.jsx";
 import "../node_modules/modern-normalize/modern-normalize.css";
 import "../node_modules/reset-css/reset.css";
 import "./index.css";
-// import { Provider } from 'react-redux'
-// import { store, persistor } from './redux/store.js'
-// import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate>
-      </Provider> */}
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
